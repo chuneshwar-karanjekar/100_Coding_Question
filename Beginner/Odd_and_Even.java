f2dc536ae32751dc06d6e3fea63b16e0a0715a5f
chuneshwar-karanjekar <chuneshwarkaranjekar@gmail.com>
2023-08-04 12:49:43 +0530

    new file added

---
+++ Beginner/Odd_and_Even.java
@@ -0,0 +1,24 @@
+
+package Beginner;
+
+import java.util.Scanner;
+
+/**
+ * Odd_and_Even
+ */
+public class Odd_and_Even {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Number : ");
+        int num = sc.nextInt();
+
+        if(num%2==0){
+            System.out.println("Your Number is Even + ");
+        }
+        else{
+            System.out.println("Your Number is Odd - ");
+        }
+
+    }
+}
