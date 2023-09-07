110959aa9b27838f0232a41d3a0b33e96f7dff8f
chuneshwar-karanjekar <chuneshwarkaranjekar@gmail.com>
2023-09-07 19:07:18 +0530

    program updated

--- Beginner/Fibonacchi.java
+++ Beginner/Fibonacchi.java
@@ -1,13 +1,22 @@
 package Beginner;
 
-public class Fibonacchi{
+import java.util.Scanner;
+
+public class Fibonacchi {
+
     public static void main(String[] args) {
-        int a=0,b=1,c;
-        for(int i=2; i<=20; i++){
-            c = a+b;
-            System.out.println(c);
-            a=b;
-            b=c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Number :");
+        int num = sc.nextInt();
+        int a = 0, b = 1;
+        int nextNum = 0;
+
+        for (int i = 2; i <= num; i++) {
+            nextNum = a + b;
+            a = b;
+            b = nextNum;
+            System.out.println(nextNum);
         }
+
     }
 }
